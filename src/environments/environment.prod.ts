32a13243d34bdb275b577caec29f9eded02ec0aa
Bertrand PETIT <bertrandpetit10@gmail.com>
2021-02-26 13:48:03 +0100

    env api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apiv2.codeattila.ch:3000/api',
-  socketUrl: 'https://apiv2.codeattila.ch:3000/',
+  apiUrl: 'https://api.codeattila.ch:3000/api',
+  socketUrl: 'https://api.codeattila.ch:3000/',
   languages: ['fr', 'en', 'de'],
 };
